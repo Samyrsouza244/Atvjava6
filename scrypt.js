bdf0836e1d0c2f2e03026ef85809dcc713d4ca7b
Samyrsouza244 <samyr1479ee@gmail.com>
2022-07-29 11:50:54 -0300

    entregue

---
+++ scrypt.js
@@ -0,0 +1,19 @@
+
+ let primeiroArray = ["Eu", "amo", "gatinhos", "sim", "amo"]; 
+
+ console.log(primeiroArray); 
+
+ const novoElemento = primeiroArray.unshift("Sim"); 
+ console.log(primeiroArray); 
+
+ const removerElemento = primeiroArray.pop(); 
+ console.log(primeiroArray); 
+
+ const novoElemento1 = primeiroArray.push("eu", "amo"); 
+ console.log(primeiroArray); 
+
+ const removerElemento1 = primeiroArray.shift(); 
+ console.log(primeiroArray); 
+
+ let numbers = [7, 5, 6, 3, 8, 9, 2, 1, 4]; 
+ console.log(numbers.sort());
